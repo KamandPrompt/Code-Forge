4641893a7ce9144ab5ffa0bf29d7de06491fc151
Tarun Srivastava <120120552+Lord-Morpheus@users.noreply.github.com>
2023-10-07 18:57:01 +0530

    Update cyclicity_in_graph.cpp
    added logic and necessary cout statements.

--- Computational_Algorithms/Graphs/cyclicity_in_graph.cpp
+++ Computational_Algorithms/Graphs/cyclicity_in_graph.cpp
@@ -1,4 +1,5 @@
 /*DISJOINT SET UNION*/
+
 /*Algorithm for finding if an undirected graph is cyclic or acyclic*/
 /*prerequisites
     -basic knowledge about graphs
@@ -22,6 +23,18 @@
     in second case we see that roads connecting ctiy 1,2,3,6 form a cycle.
 */
 
+/*LOGIC
+    let us say that if two node are connected they will have same parent. i.e. if we consider an 
+    example 1-2-3 we can say that 1 is parent of 2 and 2 is parent of 3 and since 1 is parent of 2 
+    ultimately 1 becomes the parent of 3 and obviously 1 is parent of itself.
+    therefore if we connect 1 and 3 directly we see geometrically that we will get a cycle.
+    since both already had same parent we conclude that connecting 2 nodes with same parent leads to cyclicity.
+
+    you can apply same logic while considering 2 to be the parent of 1 and 3 and then connecting 1 and 3 
+    would give us same result.
+
+*/
+
 /*ALGORITHM
     1) first label each node as its parent node. i.e. each node is parent node of itself.
     2) then traverse across the graph and find parent of each node.
@@ -35,9 +48,9 @@ using namespace std;
 int find(int node,vector<int>&parent){
     //finding the parent node
     while(parent[node]!=node){
-        node=parent[node];
+        node=parent[node]; //here since we want to find ultimate parent of that node we iterate till we get it.
     }
-    return node;
+    return node;// returning the ultimate parent of the node
 }
 
 void Union(int i,int j,vector<int>&parent){
@@ -45,30 +58,32 @@ void Union(int i,int j,vector<int>&parent){
     int iroot=find(i,parent);
     int jroot=find(j,parent);
     if(iroot!=jroot){
-        parent[jroot]=iroot;
+        parent[jroot]=iroot; //changing parent of current node from itself to its connecting node.
     }
 }
 
 bool dsu(vector<vector<int>>& edges) {
-    //setting parent of each node
+    //setting parent of each node to itself
     vector<int>parent(edges.size()+1);
     for(int i=0;i<edges.size()+1;i++)parent[i]=i;
 
     //appyling dsu
     for(auto edge:edges){
-        if(find(edge[0],parent)==find(edge[1],parent))return true;
-        Union(edge[0],edge[1],parent);
+        if(find(edge[0],parent)==find(edge[1],parent))return true;//if parent of both node is same we have a cycle.
+        Union(edge[0],edge[1],parent);//else we make one node parent of other.
     }
-    return false;
+    return false;//if no nodes have same parent then we don't have a cycle
 }
 
 int main(){
     vector<vector<int>>graph;
     int n;
+    cout<<"enter no. of nodes in your graph"<<endl;
     cin>>n;
     for (int i = 0; i < n; i++)
     {
         int a,b;
+        cout<<"enter the connected nodes i.e node1 and node2"<<endl;
         cin>>a>>b;//connected cities
         vector<int>v={a,b};
         graph.push_back(v);
@@ -80,8 +95,8 @@ int main(){
         |  |
         3--4
     */
-    bool ans= dsu(graph);
-
+    
+    bool ans= dsu(graph);//checking if graph is cyclic or not.
     if(ans)cout<<"graph is cyclic!"<<endl;
     else cout<<"graph is acyclic!"<<endl;
 }
