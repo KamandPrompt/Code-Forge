bd6220f3781acb5bcd16e2d2061d03f783be3aee
deathgod_0809 <b22149@students.iitmandi.ac.in>
2023-10-11 01:51:59 +0530

    Insertion sort using doubly linked list.

---
+++ Computational_Algorithms/Linked_Lists/Insertion_sort_doubly_linked_list.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int val){
+        data=val;
+        prev=NULL;
+        next=NULL;
+    }
+};
+void Insert_sort(Node* &head,int val){
+    Node* n=new Node(val);
+    // cout<<(head==NULL)<<endl;
+    if(head==NULL){
+        head=n;
+        // cout<<"bjh";
+        return;
+    }
+    if(head->data>val){
+        head->prev=n;
+        n->next=head;
+        head=n;
+        return;
+    }
+    // cout<<"..";
+    Node* temp=head;
+    while(temp->next!=NULL){
+        // cout<<"??";
+        if(temp->data>val){
+            n->next=temp;
+            temp->prev->next=n;
+            n->prev=temp->prev;
+            temp->prev=n;
+            // cout<<"...";
+            return;
+        }
+        else{
+            temp=temp->next;
+        }
+    }
+    // cout<<temp->data<<endl<<val;
+    if(temp->data>val){
+        n->next=temp;
+        if(temp->prev!=NULL){
+            n->prev=temp->prev;
+        temp->prev->next=n;
+        }
+        temp->prev=n;
+        // cout<<"\\";
+        return;
+    }
+    else{
+        // cout<<"..";
+        temp->next=n;
+        n->prev=temp;
+        // cout<<"<<";
+    }
+    // cout<<temp->data;
+    return;
+} 
+
+void display(Node* head){
+    Node* temp=head;
+    while(temp->next!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<(temp->data)<<endl;
+    return;
+}
+
+int main(){
+    Node* head=NULL;
+    Node* tail=NULL;
+    Insert_sort(head,20);
+    Insert_sort(head,10);
+    Insert_sort(head,50);
+    Insert_sort(head,5);
+    display(head);
+    return 0;  
+}
