390cfdf6abf55e19e55bfe84115724114576ca7f
deathgod_0809 <b22149@students.iitmandi.ac.in>
2023-10-11 10:30:53 +0530

    Added code for user inputs and Comments-2.

--- Computational_Algorithms/Linked_Lists/Insertion_sort_doubly_linked_list.cpp
+++ Computational_Algorithms/Linked_Lists/Insertion_sort_doubly_linked_list.cpp
@@ -1,3 +1,7 @@
+/*This code is of insertion sort using doubly linked list which stores address of both 
+next and previous node which helps in forward and reeverse traversa. This includes defining class Node and function for 
+insert operation and printing the doubly linked list*/ 
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -16,35 +20,34 @@ class Node{
 //Defining function for inserting a node
 void Insert_sort(Node* &head,int val){
     Node* n=new Node(val);
-    // cout<<(head==NULL)<<endl;
+    //checking for empty list
     if(head==NULL){
         head=n;
-        // cout<<"bjh";
         return;
     }
+    //if input is larger than current largest
     if(head->data>val){
         head->prev=n;
         n->next=head;
         head=n;
         return;
     }
-    // cout<<"..";
+    //Traverse till you find correct position
     Node* temp=head;
     while(temp->next!=NULL){
-        // cout<<"??";
+        //if found correct position: insert and return
         if(temp->data>val){
             n->next=temp;
             temp->prev->next=n;
             n->prev=temp->prev;
             temp->prev=n;
-            // cout<<"...";
             return;
         }
         else{
             temp=temp->next;
         }
     }
-    // cout<<temp->data<<endl<<val;
+    //checking for last element
     if(temp->data>val){
         n->next=temp;
         if(temp->prev!=NULL){
@@ -52,22 +55,19 @@ void Insert_sort(Node* &head,int val){
         temp->prev->next=n;
         }
         temp->prev=n;
-        // cout<<"\\";
         return;
     }
     else{
-        // cout<<"..";
         temp->next=n;
         n->prev=temp;
-        // cout<<"<<";
     }
-    // cout<<temp->data;
     return;
 } 
 
 //Defining function for display doubly linked list
 void display(Node* head){
     Node* temp=head;
+    //Traversing through nodes
     while(temp->next!=NULL){
         cout<<temp->data<<" ";
         temp=temp->next;
@@ -80,10 +80,20 @@ void display(Node* head){
 int main(){
     Node* head=NULL;
     Node* tail=NULL;
-    Insert_sort(head,20);
-    Insert_sort(head,10);
-    Insert_sort(head,50);
-    Insert_sort(head,5);
+    //Code for taking input from user
+    cout<<"Enter number of inputs:";
+    int n;cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"Enter a number:";
+        int x;cin>>x;
+        Insert_sort(head,x);
+    }
+    cout<<"Sorted list: ";
     display(head);
     return 0;  
 }
+
+/*
+Code contributed by-Abhijeet Jha
+B22149
+*/
