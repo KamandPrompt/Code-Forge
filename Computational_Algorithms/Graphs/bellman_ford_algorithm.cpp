cad5fae78b305521e84c2446adfbf720bb4a0f77
deathgod_0809 <b22149@students.iitmandi.ac.in>
2023-10-11 14:28:46 +0530

    Added comments for the code.

--- Computational_Algorithms/Graphs/bellman_ford_algorithm.cpp
+++ Computational_Algorithms/Graphs/bellman_ford_algorithm.cpp
@@ -1,23 +1,27 @@
+//This code is of Bellman-ford algorithm for graphs which finds the shortest distance of vertices from the source provided.
+//This code also detects negative cycle and prompts the user about the same.
 #include <bits/stdc++.h>
-
-
 using namespace std;
 
+//Defining edge class with its three attributes
 class Edge {
 public:
     int source;
     int destination;
     int weight;
-
+//definig initial parameter assignments
     Edge(int src, int dest, int wt) : source(src), destination(dest), weight(wt) {}
 };
 
+//Defining class for algorithm
 class BellmanFord {
 public:
     void bellmanFord(vector<Edge>& edges, int numVertices, int source) {
+        //setting distance to maximum so that we can change later
         vector<int> distances(numVertices, INT_MAX);
+        //distance of source =0
         distances[source] = 0;
-
+        //checking for distance and assigning new distance if its less than current distance
         for (int i = 1; i <= numVertices - 1; ++i) {
             for (const auto& edge : edges) {
                 int u = edge.source;
@@ -47,7 +51,9 @@ public:
     }
 };
 
+//Main code
 int main() {
+    //Defining edges of the graph
     vector<Edge> edges = {
         {0, 1, 6},
         {0, 2, 2},
@@ -57,6 +63,7 @@ int main() {
         {3, 4, -3},
         {4, 3, 3}
     };
+    //defining source node
     int numVertices = 5;
     int sourceNode = 0;
 
@@ -65,3 +72,8 @@ int main() {
 
     return 0;
 }
+/*
+Code conrivuted by:
+Abhijeet Kumar Jha
+B22149
+*/
