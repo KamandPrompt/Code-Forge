38bdfbc4283c9bbea36cec1e0a8644aff285c9d2
coderbeta1 <achaljain232@gmail.com>
2023-10-12 20:56:48 +0530

    Added searching using Tries application

---
+++ Applications/Computational_Algorithm_Applications/search_prediction.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_map<string,int> counter;
+
+// Node Definition
+class TrieNode{
+    public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal=0;
+
+    TrieNode(char ch){
+        data=ch;
+        for(int i=0;i<26;i++){
+            children[i]=NULL;
+        }
+        isTerminal=0;
+    }
+};
+
+// Class Functions
+class Trie{
+    public:
+    TrieNode* root;
+
+    Trie(){
+        root=new TrieNode('\0');
+    }
+
+    void InsertUtil(TrieNode* root, string word){
+        if(word.length()==0){
+            root->isTerminal=true;
+            return;
+        }
+
+        int index= word[0]-'a';
+        TrieNode* child;
+
+        if(root->children[index]!=NULL){
+            child=root->children[index];
+        }
+        else{
+            child=new TrieNode(word[0]);
+            root->children[index]=child;
+        }
+
+        InsertUtil(child,word.substr(1)); 
+    }
+
+    void InsertWord(string word){
+        InsertUtil(root,word);
+    }
+
+    void inorderTravel(TrieNode* root, vector<string> &found, string current){
+        if(root == NULL){
+            return;
+        }
+
+        current.push_back(root->data);
+
+        if(root->isTerminal){
+            found.push_back(current);
+        }
+
+        for(int i=0;i<26;i++){
+            inorderTravel(root->children[i], found, current);
+        }
+        current.pop_back();
+        return;
+    }
+
+    void SearchUtill(TrieNode* root,string word, vector<string> &found, string currentStr=""){
+        if(word.length()==0){
+            if(currentStr.length()>=1) currentStr.pop_back();
+            inorderTravel(root, found, currentStr);
+            return;
+        }
+
+        int index=word[0]-'a';
+        TrieNode* child;
+
+        currentStr.push_back(word[0]);
+        if(root->children[index]!=NULL){
+            child = root -> children[index];
+        }
+        else{
+            return;
+        }
+
+        return SearchUtill(child, word.substr(1), found, currentStr);
+    }
+
+    vector<string> Search(string word){
+        vector<string> found; 
+        SearchUtill(root,word,found);
+        return found;
+    }
+
+    void RemoveUtil(TrieNode* root,string word){
+        if(word.length()==0){
+            root->isTerminal=false;
+            return;
+        }
+
+        int index= word[0]-'a';
+        TrieNode* child;
+
+        if(root->children[index]!=NULL){
+            child=root->children[index];
+        }
+        else{
+            return;
+        }
+
+        RemoveUtil(child,word.substr(1)); 
+    }
+
+    void Remove(string word){
+        RemoveUtil(root,word);
+    }
+};
+
+// Freq Comparision
+bool comparator(string a,string b){
+    return counter[a]>counter[b];
+}
+
+int main(){
+    // Size
+    cout<<"Number of Words: ";
+    int n;
+    cin>>n;
+
+    // Insertion
+    cout<<"Enter Words:\n";
+    Trie* T=new Trie();
+    for(int i=0;i<n;i++){
+        string inp;
+        cin>>inp;
+
+        counter[inp]+=1;
+        T->InsertWord(inp);
+    }
+
+    // Search
+    cout<<"\nWord to find: ";
+    string tocheck;
+    cin>>tocheck;    
+    vector<string> found = T -> Search(tocheck);
+    sort(found.begin(),found.end(),comparator);
+
+    // Printing
+    cout<<"\nSearch Result: \n";
+    for(auto i:found){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
