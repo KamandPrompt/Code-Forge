b9edee3576c53ccbb9d3f95198b1de80e2c25dd7
vivek005001 <vivekaggarwal569@gmail.com>
2023-10-14 12:26:32 +0530

    updated bfs
    added comment for time complexity and some explanation

--- Computational_Algorithms/Graphs/bfs.cpp
+++ Computational_Algorithms/Graphs/bfs.cpp
@@ -3,7 +3,17 @@
 #include <queue>
 #include <list>
 using namespace std;
+//This C++ program is designed to perform Breadth-First Search (BFS) on an undirected graph.
+//  BFS is a graph traversal algorithm that explores all the vertices of the graph in breadth-first order
+//  starting from a given source vertex.
 
+
+
+// In terms of time complexity, constructing the adjacency list takes O(E) time, where E is the number of edges.
+// The BFS traversal, in the worst case, has a time complexity of O(V + E), where V is the number of vertices and E is the number of edges.
+// The overall time complexity of the BFS algorithm in this code is O(V + E).
+
+This code can be useful for exploring and analyzing undirected graphs, and it can serve as a foundation for various graph-related tasks, such as finding connected components or shortest paths.
 void create_AdjList(unordered_map<int, set<int>>& adjList, vector<pair<int, int>>& edges) {
     for (int i = 0; i < edges.size(); i++) {
         int u = edges[i].first;
