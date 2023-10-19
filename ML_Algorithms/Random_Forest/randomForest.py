c4d694825a4ac9a45c55fd1bedff7b9cf0cae9cd
Souradeep <isouradeephazra@gmail.com>
2023-10-19 13:22:28 +0530

    Added Random forest

---
+++ ML_Algorithms/Random_Forest/randomForest.py
@@ -0,0 +1,26 @@
+# Import the necessary libraries
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load a dataset for demonstration (Iris dataset)
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Create a Random Forest classifier
+clf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the classifier on the training data
+clf.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = clf.predict(X_test)
+
+# Calculate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
