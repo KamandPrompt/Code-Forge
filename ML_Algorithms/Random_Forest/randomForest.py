5d09663f7289e1ea8019c4e252107fe7e6153f54
Souradeep <isouradeephazra@gmail.com>
2023-10-20 18:54:47 +0530

    Added explaination at the top of .py file

--- ML_Algorithms/Random_Forest/randomForest.py
+++ ML_Algorithms/Random_Forest/randomForest.py
@@ -1,3 +1,11 @@
+
+#explaination:-
+# This code is an implementation of a Random Forest classifier using the scikit-learn library in Python. The code first imports the necessary libraries, including datasets, train_test_split, RandomForestClassifier, and accuracy_score. The Iris dataset is then loaded and split into training and testing sets using the train_test_split function.
+
+# Next, a Random Forest classifier is created with 100 estimators and a random state of 42. The classifier is then trained on the training data using the fit method. Predictions are made on the test data using the predict method, and the accuracy of the model is calculated using the accuracy_score function. Finally, the accuracy is printed to the console.
+
+# This code is a simple and straightforward implementation of a Random Forest classifier using scikit-learn. It demonstrates how to load a dataset, split it into training and testing sets, create a classifier, train the classifier, make predictions, and calculate the accuracy of the model.
+
 # Import the necessary libraries
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
