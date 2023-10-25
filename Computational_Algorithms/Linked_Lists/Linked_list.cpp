6d43bac5a40a955ce184ff9cbcadd1fc13c449a6
deathgod_0809 <b22149@students.iitmandi.ac.in>
2023-10-25 20:14:20 +0530

    Added comments and corner case

--- Computational_Algorithms/Linked_Lists/Linked_list.cpp
+++ Computational_Algorithms/Linked_Lists/Linked_list.cpp
@@ -1,10 +1,18 @@
 
 /*
 This is a cpp code for linked list.
+Code includes following functions:
+1.)We can insert front to a linked list.
+    Time complexity:O(1)
+2.)We can insert tail to a linked list.
+    Time complexity:O(1)
+3.)Print the linked list.We will start from head and traverse through the linked list.
+    Time Complexity: O(n)
 */
 #include<bits/stdc++.h>
 using namespace std;
 
+//Creating a node class with properties data and next address.
 class Node{
     public:
     int data;
@@ -15,14 +23,17 @@ class Node{
     }
 };
 
+//Creating function to add tail
 void insertTail(Node* &head,int val,Node* &tail){
     Node* n =new Node(val);
+    //Checking for empty list.
     if(head==NULL){
         head=n;
         tail=n;
         //cout<<head<<endl;
         return;
     }
+    //Add tail
     Node* temp=head;
     while(temp->next!=NULL){
         temp=temp->next;
@@ -31,21 +42,32 @@ void insertTail(Node* &head,int val,Node* &tail){
     tail=n;
 }
 
+//function to insert head
 void Insert_Head(Node* &head,int val){
     Node* n= new Node(val);
     n->next=head;
     head=n;
 }
+
+//Code to print the linked list
 void display(Node* head){
-    Node* temp=head;
-    while(temp->next !=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+    //checking for empty list
+    if(head==NULL){
+        cout<<"List is empty!!"<<endl;
     }
-    cout<<temp->data;
-    cout<<endl;
+    else{
+        Node* temp=head;
+        while(temp->next !=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<temp->data;
+        cout<<endl;
+    }
+    
 }
 
+//Driver code
 int main(){
     Node* head=NULL;
     Node* tail=NULL;
