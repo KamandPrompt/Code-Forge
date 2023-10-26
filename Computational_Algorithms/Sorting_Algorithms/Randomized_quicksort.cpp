e2de7dd60cc4ce051ca2155d43dfa29e98853a4e
Rudra-IITM <singhrudra5556@gmail.com>
2023-10-26 22:04:45 +0530

    Add comments to randomized quicksort

--- Computational_Algorithms/Sorting_Algorithms/Randomized_quicksort.cpp
+++ Computational_Algorithms/Sorting_Algorithms/Randomized_quicksort.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// Heapsort using cpp STL
 void heapSort(int arr[],int low,int high){
     make_heap(arr+low,arr+high+1);
     sort_heap(arr+low,arr+high+1);
 }
 
+// Function to find random pivot element 
 int partition(int arr[],int low,int high){
     int pivot=(rand()% (high-low+1)) +low;
     int pivot_element=arr[pivot];
@@ -21,9 +23,11 @@ int partition(int arr[],int low,int high){
     return r;
 }
 
+// Quicksort
 void quickSort(int arr[],int low,int high,int mxD,int subP){
     if (high>low){
         if (mxD==0){
+            // Calls HeapSort if threshold is reached
             heapSort(arr,low,high);
             return;
         }
@@ -41,6 +45,8 @@ int main(){
     for (int i=0;i<n;i++) cin>>arr[i];
     int mxD=2*log2(n),subP=0;
     quickSort(arr,0,n-1,mxD,subP);
+
+    // Print sorted array
     for (int i=0;i<n;i++) cout<<arr[i]<< " ";
     cout<<endl;
     return 0;
