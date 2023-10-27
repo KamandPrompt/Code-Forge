e9f2b3d9c788b04bfe21067b5444e65a774eb573
nishchay2517 <114824970+nishchay2517@users.noreply.github.com>
2023-10-27 16:59:03 +0530

    added comments

--- Computational_Algorithms/Binary_exponentiation/binary_exponentiation.cpp
+++ Computational_Algorithms/Binary_exponentiation/binary_exponentiation.cpp
@@ -1,20 +1,22 @@
 #include<iostream>
-#define ll long long 
+#define ll long long //we define ll for long long data type
 using namespace std;
 
 ll power(ll x ,ll n){
+    //we consider x as base and n as exponent
     ll ans;
     if(n==0){
-        return 1;
+        return 1; //if exponent is 0 then result will be 1
     }
     if (n%2==0)
     {
+        // if n is then the property 4^4 = (4^2)*(4^2) can be used  
        ans = power(x , n/2)*power(x , n/2);
     }
     else{
+        // if n is odd then the property 4^5 = 4*(4^2)*(4^2) can be used
         ans = x*power(x,(n-1)/2)*power(x,(n-1)/2); 
     }
-    // cout<<ans;
 return ans;
  
 }
